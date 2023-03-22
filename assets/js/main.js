a2faefbec9c2f729be0a2825ea67d911b0399eea
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-22 13:54:35 +0100

    ADD Array of team members and print to console

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,61 @@ Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
 Scott Estrada	Developer	scott-estrada-developer.jpg
 Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
 */
+
+// Create an array containing the properties of each team member
+const teamMembers = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    photo: "wayne-barnett-founder-ceo.jpg"
+  },
+
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    photo: "angela-caroll-chief-editor.jpg"
+  },
+
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    photo: "walter-gordon-office-manager.jpg"
+  },
+
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    photo: "angela-lopez-social-media-manager.jpg"
+  },
+
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    photo: "scott-estrada-developer.jpg"
+  },
+
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    photo: "barbara-ramos-graphic-designer.jpg"
+  }
+]
+
+// Print, for each team member, all the properties he has 
+logToConsole(teamMembers);
+
+// <---------- FUNCTIONS ---------->
+
+/**
+ * Prints to console each property of every team member
+ * @param {object[]} teamMembers Array containing every team member, with its properties
+ */
+function logToConsole(teamMembers) {
+  for (let i = 0; i < teamMembers.length; i++) {
+    const thisMember = teamMembers[i];
+    console.log(`Name: ${thisMember.name}`);
+    console.log(`Role: ${thisMember.role}`);
+    console.log(`Photo: ${thisMember.photo}`);
+    console.log(`----------`);
+  }
+}
